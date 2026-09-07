57f26d5150f843bf2fd9093006f9c2abf20793f5
lakshaysoni22 <lakshaysoni22@users.noreply.github.com>
2026-09-07 18:39:55 +0530

    fix: auto unregister legacy service worker and clear pwa cache

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+// Self-destructing Service Worker to purge old LeatTrace PWA cache
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((k) => caches.delete(k)));
+    }).then(() => {
+      return self.registration.unregister();
+    }).then(() => {
+      return self.clients.claim();
+    }).then(() => {
+      return self.clients.matchAll({ type: 'window' });
+    }).then((clients) => {
+      clients.forEach((client) => client.navigate(client.url));
+    })
+  );
+});
